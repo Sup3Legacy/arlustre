a5a1d62783928d470b6a5d7ffa02b8b84681ee26
Constantin Gierczak--Galle <constantin.gierczak-galle@ens.fr>
2021-11-09 15:14:36 +0100

    Some test files

---
+++ src/test_c/test.h
@@ -0,0 +1,15 @@
+/* --- Generated the 9/11/2021 at 0:3 --- */
+/* --- heptagon compiler, version 1.05.00 (compiled mon. sep. 27 15:58:3 CET 2021) --- */
+/* --- Command line: /home/constantin/.opam/4.12.0/bin/heptc -target c test.lus --- */
+
+#ifndef TEST_H
+#define TEST_H
+
+#include "test_types.h"
+typedef struct Test__main_out {
+  int o;
+} Test__main_out;
+
+void Test__main_step(Test__main_out* _out);
+
+#endif // TEST_H
---
+++ src/test_c/test_types.c
@@ -0,0 +1,9 @@
+/* --- Generated the 9/11/2021 at 0:3 --- */
+/* --- heptagon compiler, version 1.05.00 (compiled mon. sep. 27 15:58:3 CET 2021) --- */
+/* --- Command line: /home/constantin/.opam/4.12.0/bin/heptc -target c test.lus --- */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "test_types.h"
+
