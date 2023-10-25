15566da761f691b6f8d817807676ae305c4a0265
Constantin Gierczak--Galle <git.cst1@mailbox.org>
2023-10-25 14:29:50 +0200

    Init simple Nix build

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "ArLustre";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            zig
+            avrdude
+            screen
+
+            ocaml
+            ocamlPackages.alcotest
+            ocamlPackages.findlib
+            ocamlPackages.menhir
+            ocamlPackages.menhirLib
+            ocamlPackages.ocamlgraph
+            ocamlPackages.camlp4
+            ocamlPackages.ocamlbuild
+          ];
+        };
+      });
+}
+
